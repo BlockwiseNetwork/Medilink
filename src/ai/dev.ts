6d4509b26628680e6e809bf4eb8f2ede6dffb2c8
Testimony Edem <testimonyedem01@gmail.com>
2025-10-23 13:45:34 +0000

    the language is not working make it work

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,5 +2,4 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/medibot-ai-health-assistant.ts';
-import '@/ai/flows/medibot-respond-in-ibibio.ts';
 import '@/ai/flows/medibot-find-doctors-in-city.ts';
